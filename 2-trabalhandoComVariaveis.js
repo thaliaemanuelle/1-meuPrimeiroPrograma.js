5247193609cf5ac17c890152615c361b9bb19276
thaliaemanuelle <129748042+thaliaemanuelle@users.noreply.github.com>
2023-09-12 07:55:15 -0300

    Update 2-trabalhandoComVariaveis.js

--- 2-trabalhandoComVariaveis.js
+++ 2-trabalhandoComVariaveis.js
@@ -6,4 +6,12 @@ const idade = 29;
 const nome = "Ricardo";
 
 //não fazer
+//mes = "Janeiro";//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 29;
+const nome = "Ricardo";
+
+//não fazer
 //mes = "Janeiro";
